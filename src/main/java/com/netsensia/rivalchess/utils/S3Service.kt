58c5766f84c6e9f33f47f4ef9ef7ae9e706f21ef
Chris Moreton <chris@netsensia.com>
2020-10-21 15:50:58 +0100

    Tagging as 17.0.0

--- src/main/java/com/netsensia/rivalchess/utils/S3Service.kt
+++ src/main/java/com/netsensia/rivalchess/utils/S3Service.kt
@@ -11,6 +11,24 @@ val accessKey = System.getenv("AWS_ACCESS_KEY_ID")
 val secretKey = System.getenv("AWS_SECRET_ACCESS_KEY")
 val endpoint = System.getenv("S3_ENDPOINT")
 
+fun getEngine(
+        s3Client: AmazonS3Client,
+        engineVersion: String
+) {
+    val s3Name = "rivalchess-${engineVersion}-1.jar"
+    val filePath = "/tmp/$s3Name"
+
+    if (File(filePath).exists()) {
+        println("$s3Name already exists")
+    } else {
+        println("Getting engine $engineVersion from S3")
+        val o = s3Client.getObject(GetObjectRequest("rivalchess-jars", s3Name))
+        val objectData: InputStream = o.objectContent
+        File(filePath).writeBytes(objectData.readBytes())
+        objectData.close()
+    }
+}
+
 fun getS3Client(): AmazonS3Client {
     val credentials = BasicAWSCredentials(accessKey, secretKey)
 
