e5a023a1aef88e3c34f4d152f1ad97d9934147bb
Chris Moreton <chris@netsensia.com>
2020-11-01 17:57:54 +0000

    Tagging as 27.0.1

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Reads messages from the Rival queue, plays the requested matches and posts the r
     <dependency>
         <groupId>com.netsensia.rivalchess</groupId>
         <artifactId>rivalchess-vie-utils</artifactId>
-        <version>'27.0.0'</version>
+        <version>'27.0.1'</version>
     </dependency>
     
 #### Gradle
 
-    compile group: 'com.netsensia.rivalchess', name: 'rivalchess-vie-utils', version: '27.0.0'
+    compile group: 'com.netsensia.rivalchess', name: 'rivalchess-vie-utils', version: '27.0.1'
 
--- src/main/java/com/netsensia/rivalchess/utils/mock/MockJmsSender.kt
+++ src/main/java/com/netsensia/rivalchess/utils/mock/MockJmsSender.kt
@@ -1,12 +1,17 @@
 package com.netsensia.rivalchess.utils.mock
 
+import com.netsensia.rivalchess.utils.interfaces.JmsServiceInterface
 import com.netsensia.rivalchess.vie.model.EngineMatch
 
-object MockJmsSender : JmsSenderInterface {
+object MockJmsSender : JmsServiceInterface {
     val engineSettingsList = mutableListOf<EngineMatch>()
 
     override fun send(subject: String, message: Any) {
         val engineSettingsMessage = message as EngineMatch
         engineSettingsList.add(engineSettingsMessage)
+    }
+
+    override fun receive(subject: String): String {
+        return "IAmAMock"
     }
 }
